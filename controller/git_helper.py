ed4f65ac01f7b973e4cb1e164816258f068608b3
ludwig778 <laurent.arthur75@gmail.com>
2017-01-10 16:15:37 +0100

    Git pull/clone on the right path with the right branch

--- controller/git_helper.py
+++ controller/git_helper.py
@@ -5,11 +5,16 @@ BASE_REPOS_PATH = os.path.abspath(os.path.join("/", "var", "opt", "git_repos"))
 
 def manage_repo(url, repo, branch):
     print url, repo, branch
+    checkout_to = branch
     if False:
+        if branch not in ["master", "preprod"]:
+            branch = "test"
         repo_folder = os.path.join(BASE_REPOS_PATH, repo, branch)
          
         if not os.path.exists(repo_folder):
             Repo.clone_from("git_url", repo_folder)
+            repo = Git(repo_folder)
         else:
-            repo = git.cmd.Git(repo_folder)
+            repo = Git(repo_folder)
             repo.pull()
+        repo.checkout(checkout_to)
