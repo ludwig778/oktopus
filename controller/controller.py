70d7102187920bca5f2528fedb72370cb6755806
ludwig778 <laurent.arthur75@gmail.com>
2017-01-17 14:47:05 +0100

    Fix #5

--- controller/controller.py
+++ controller/controller.py
@@ -168,7 +168,7 @@ class Controller:
         print "good 2"
         datas = ""
         for i in self.datas['apps']:
-            if self.datas['apps'][i]['name'] == repo:
+            if self.datas['apps'][i]['git']['name'] == repo:
                 datas = self.datas['apps'][i]
 
         if not datas:
